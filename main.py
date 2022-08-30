64bf2354a3fc866fb781373e35f607b9371e50e8
koushik <koushik.dey@live.com>
2022-08-30 17:25:33 +0300

    changed layout

--- main.py
+++ main.py
@@ -9,18 +9,25 @@ from kivy.uix.button import Button
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text=" First Name: "))
+        self.cols = 1
+
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+
+        self.inside.add_widget(Label(text=" First Name: "))
         self.firstName = TextInput(multiline=False)
-        self.add_widget(self.firstName)
+        self.inside.add_widget(self.firstName)
 
-        self.add_widget(Label(text="Last Name: "))
+        self.inside.add_widget(Label(text="Last Name: "))
         self.lastName = TextInput(multiline=False)
-        self.add_widget(self.lastName)
+        self.inside.add_widget(self.lastName)
 
-        self.add_widget(Label(text="Email: "))
+        self.inside.add_widget(Label(text="Email: "))
         self.email = TextInput(multiline=False)
-        self.add_widget(self.email)
+        self.inside.add_widget(self.email)
+
+        self.add_widget(self.inside)
 
         self.submit = Button(text="Submit", font_size=40)
         self.add_widget(self.submit)
